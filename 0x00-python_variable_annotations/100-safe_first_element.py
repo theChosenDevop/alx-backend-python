367b9f16ba1288429506bba06e2017bbab5fe647
root <root@ChosenDevop.localdomain>
2024-05-03 00:17:03 +0100

    Sequence typing

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,8 +4,10 @@
     Defines 100-safe_first_element module
 """
 
+from typing import Sequence, Union, Any
 
-def safe_first_element(lst):
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     if lst:
         return lst[0]
     else:
