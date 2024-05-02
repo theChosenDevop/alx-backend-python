6a04b170df2ca69f80d7e41d6bab6e1086739db2
root <root@ChosenDevop.localdomain>
2024-05-02 19:49:51 +0100

    add function annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+""" Defines Add function module"""
+
+def add(a: float, b: float) -> float :
+    """Adds two float numbers
+
+    Arguments:
+        a {float} -- float
+        b {float} -- float
+    Returns:
+        float --- A float number
+    """    ""
+    return (a + b)
