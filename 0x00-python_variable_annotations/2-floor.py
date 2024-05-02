e502614f86aa38c05505f213db15ecf4f3ebc4bf
root <root@ChosenDevop.localdomain>
2024-05-02 20:46:13 +0100

    annotate str function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Defines floor function module
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """ returns a float of floor n"""
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    Defines 3-to_str module
+"""
+
+
+def to_str(n: float) -> str:
+    """"returns string representation of n """
+    return str(n)
