f7fceffb839e90fc499a7e9248c4a151482f72d7
theChosenDevop <tobbey.adesanya@gmail.com>
2024-05-31 19:35:31 +0000

    test memoize function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """test_utils module"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import *
 from unittest.mock import patch, Mock
 
 
@@ -47,6 +47,35 @@ class TestGetJson(unittest.TestCase):
         mock_get.assert_called_once_with(test_url)
 
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test case for test_memoize method"""
+
+    def test_memoize(self):
+        """test memoize function"""
+        class TestClass:
+            """Test Class"""
+
+            def a_method(self):
+                """returns an integer"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """returns property of object"""
+                return self.a_method()
+        obj_test = TestClass()
+
+        with patch.object(
+                TestClass, 'a_method', return_value=42) as mock_method:
+            result1 = obj_test.a_property
+            result2 = obj_test.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+            mock_method.assert_called_once()
 
 
 if __name__ == "__main__":
