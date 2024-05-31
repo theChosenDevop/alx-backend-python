fea037b26dab804cf8e8086da8abdd02d65a2c47
theChosenDevop <tobbey.adesanya@gmail.com>
2024-05-31 21:15:39 +0000

    update

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -14,15 +14,18 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google", {"login": "google"}),
         ("abc", {"login": "abc"})
         ])
-    @patch('client.getjson')
+    @patch('client.get_json')
     def test_org(self, org_name: str,
                  response: Dict, mock_get_json) -> None:
         """Test org if the method return expected result"""
         mock_get_json.return_value = response
 
         client = GithubOrgClient(org_name)
-        result = client.org()
+        result = client.org
 
         self.assertEqual(result, response)
         mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/{}".format(org_name))
+
+if __name__ == "__main__":
+    unittest.main()
