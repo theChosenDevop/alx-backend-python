979f8a5d02dece05e9be1d0b609d86f6d624dc2d
theChosenDevop <tobbey.adesanya@gmail.com>
2024-07-31 16:49:46 -0700

    measure time to compute integers

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -15,5 +15,5 @@ async def measure_time(n: int, max_delay: int) -> float:
         Returns: execution time
     """
     start_time = time.time()
-    asyncio.run(wait_n(n, max_delay))
+    await wait_n(n, max_delay)
     return (time.time() - start_time) / n
