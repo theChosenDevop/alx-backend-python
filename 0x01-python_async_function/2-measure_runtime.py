8a2217a0fe6641e89cf5230b2314e0ae449273c7
theChosenDevop <tobbey.adesanya@gmail.com>
2024-07-31 16:46:53 -0700

    measure time to compute integers

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -3,7 +3,7 @@
 import asyncio
 import time
 
-wait_n = __import__('1-concurrent_coroutines')
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+wait_random = __import__('')
+def task_wait_random() :
+    """"""
